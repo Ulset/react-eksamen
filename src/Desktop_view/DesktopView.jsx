d2c3256a34061ee7d55f938dc9185495e55f16e5
Sander Ulset <sanderulset1@gmail.com>
2020-11-03 19:52:09 +0100

    Fjerna drit

---
+++ src/Desktop_view/DesktopView.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Desktop_view/Desktop.css'
+class DesktopView extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render = () => {
+        return (
+            <div>
+                <p>Desktop</p>
+            </div>
+        )
+    }
+}
+
+export default DesktopView
---
+++ src/Mobile_view/MobileView.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class MobileView extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render = () => {
+        return (
+            <p>Mobil</p>
+        )
+    }
+}
+
+export default MobileView
