bdefd8e3227c6a22e1d5e29e686520bd37d29241
Sander Ulset <sanderulset1@gmail.com>
2020-11-04 02:26:50 +0100

    kan åpne shoppingcart, ingenting der enda

--- src/Mobile_view/Components/ShoppingCart.jsx
+++ src/Mobile_view/Components/ShoppingCart.jsx
@@ -6,14 +6,33 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 class ShoppingCart extends React.Component {
     constructor(){
         super();
+        this.state = {
+            opened: false
+        }
+    }
+
+    openShoppingCart = () => {
+        this.setState({
+            opened: true
+        })
+    }
+
+    openedShoppingCartContent = () => {
+        return (
+            <div>
+                
+            </div>
+        )
     }
 
     render = () => {
         return (
-            <div className="mobile_shoppingcart_closed">
-                <div className="logo_container">
-                    <FontAwesomeIcon icon={faShoppingCart} size="2x" className="mobile_shopping_cart_icon" />
-                </div>
+            <div className={`mobile_shoppingcart${this.state.opened ? ' opened' : ''}`} onClick={this.openShoppingCart}>
+                {!this.state.opened ? 
+                    <div className="logo_container">
+                        <FontAwesomeIcon icon={faShoppingCart} size="2x" className="mobile_shopping_cart_icon" />
+                    </div>
+                : this.openedShoppingCartContent()}
             </div>
         )
     }
