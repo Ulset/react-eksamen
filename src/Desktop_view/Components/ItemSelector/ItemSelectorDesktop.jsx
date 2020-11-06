8d7005f770807687d265cbd87763a246b1788fa9
Sander Ulset <sanderulset1@gmail.com>
2020-11-06 22:41:19 +0100

    starter på cart dynamikk

--- src/Desktop_view/Components/ItemSelector/ItemSelectorDesktop.jsx
+++ src/Desktop_view/Components/ItemSelector/ItemSelectorDesktop.jsx
@@ -3,8 +3,15 @@ import CategorySelector from './CategorySelector';
 import ItemComponent from './ItemComponent'
 
 class ItemSelectorDesktop extends React.Component {
-    constructor(){
+    constructor(props){
         super();
+    }
+
+    getItems = ()=> {
+        return this.props.items.map((el, key) => {
+            let isPLacedInCart = this.props.cart.includes(el);
+            return <ItemComponent item={el} key={key} addToCartFunc={this.props.addToCart} removeFromCart={this.props.removeFromCart} isPlacedInCart={isPLacedInCart}/>
+        })
     }
 
     render = () => {
@@ -12,16 +19,7 @@ class ItemSelectorDesktop extends React.Component {
             <div className="desktop_item_selector_container desktop_main_content">
                 <CategorySelector />
                 <div className="desktop_item_components_container">
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
-                    <ItemComponent item={this.props.items[0]}/>
+                    {this.getItems()}
                 </div>
             </div>
         )
--- src/Desktop_view/DesktopView.jsx
+++ src/Desktop_view/DesktopView.jsx
@@ -7,13 +7,46 @@ import ShoppingCartDesktop from './Components/ShoppingCartDesktop'
 class DesktopView extends React.Component {
     constructor(props){
         super();
+        this.state = {
+            shoppingCart: []
+        }
+    }
+
+    addToShoppingCart = (itemEl) => {
+        let cartNow = this.state.shoppingCart
+        cartNow.push(itemEl)
+        this.setState({
+            shoppingCart: cartNow
+        })
+    }
+
+    removeFromShoppingCart = (itemEl) => {
+        let cartNow = this.state.shoppingCart
+        cartNow.map((el, index) => {
+            if(itemEl.name == el.name){
+                cartNow.splice(index, 1)
+            }
+        })
+        this.setState({
+            shoppingCart: cartNow
+        })
+    }
+
+    emptyShoppingCart = () => {
+        this.setState({
+            shoppingCart: []
+        })
     }
 
     render = () => {
         return (
             <div className="desktop_view_body">
                 <div className="desktop_view_content_container">
-                    <ItemSelectorDesktop  items={this.props.items}/>
+                    <ItemSelectorDesktop  
+                        items={this.props.items}
+                        addToCart={this.addToShoppingCart}
+                        removeFromCart={this.removeFromShoppingCart} 
+                        cart={this.state.shoppingCart} />
                     <WorkerTab  />
                     <ShoppingCartDesktop  />
                 </div>
