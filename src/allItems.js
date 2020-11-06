ba9a0ab12bd9e6ff88b5b988208817ebce70efb3
Sander Ulset <sanderulset1@gmail.com>
2020-11-06 16:36:03 +0100

    Riktig bilder på alt

--- src/allItems.js
+++ src/allItems.js
@@ -4,70 +4,70 @@ let coffe = [
         desc: "Denne har du smakt før",
         price: 32,
         cat: "kaffe",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://www.metallskilt.no/users/metallskilt_mystore_no/images/88831_NA_Coffee_First_Your_Bullshit_Second_6.png"
     },
     {
         name: "Cappuccino",
         desc: "Feelin fancy are ya",
         price: 42,
         cat: "kaffe",
-        img: "https://cdn5.mystore4.no/thumb/600_600/metallskilt/88831_NA_Coffee_First_Your_Bullshit_Second_8.png"
+        img: "https://pornovape.com/images/thumbnails/250/250/detailed/1/Cappuccino@2x.png"
     },
     {
         name: "Americano",
         desc: "Når du ikke er helt tøff nok for espresso",
         price: 36,
         cat: "kaffe",
-        img: "https://www.nostalgia.no/users/nostalgia_mystore_no/images/45487_Nostalgic_Art_Krus_-_Have_A_Coffee_1.png"
+        img: "https://mixcafe.co/wp-content/uploads/2017/09/americano-mix.png"
     },
     {
         name: "Iskaffe",
         desc: "Varm dag?",
         price: 42,
         cat: "kaffe",
-        img: "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Treats/Treats%20PDP/_0000s_0004_%5BFeed%5D_0005s_0000_Treats_Frosted-Coffee.png"
+        img: "https://www.monin.com/us/media/catalog/product/cache/715e3bd7e68644be70dcb763021c03d6/8/f/8fa59f07-1232-4009-ab39-b7c32ea3b5d0-2.png"
     },
     {
         name: "Caffè Mocha",
         desc: "Kaffe på lørdagskveld",
         price: 46,
         cat: "kaffe",
-        img: "https://cdn3.mystore4.no/thumb/600_600/metallskilt/54975_NA_All_Types_of_Coffee_1.png"
+        img: "https://i.pinimg.com/originals/84/91/28/849128e37ef58ebecbeb3cd5febd00f6.png"
     },
     {
         name: "Caffè Latte",
         desc: "Flere kalorier en du tror!",
         price: 48,
         cat: "kaffe",
-        img: "https://kuacoffee.co/static/14c0c0e596800b7693e5d9635458f8c5/ddb9e/cup-4.png"
+        img: "https://img.pngio.com/download-free-png-caffe-latte-dlpngcom-latte-png-400_276.png"
     },
     {
         name: "Macchiato",
         desc: "Søt, men også fyldig",
         price: 51,
         cat: "kaffe",
-        img: "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_300,h_300/https://www.coffeeworks.co.th/wp-content/uploads/2018/07/coffee-cup-small-lighter_03-300x300.png"
+        img: "https://cdn.shopify.com/s/files/1/0016/2188/4964/products/lattemacchiato_588x540.png?v=1584633491"
     },
     {
         name: "Espresso",
         desc: "Jeg må våkne - NÅ",
         price: 38,
         cat: "kaffe",
-        img: "https://brewcoffeehouse.com/wp-content/uploads/2020/07/coffee.png"
+        img: "https://www.philips.no/c-dam/b2c/category-pages/Household/coffee/master/faq/cups/Productimg_Lungo.png"
     },
     {
         name: "Cortado",
         desc: "Eksotisk!",
         price: 40,
         cat: "kaffe",
-        img: "https://kuacoffee.co/static/06b311464afb29df3392f8d9d8738ed0/ddb9e/cup-2.png"
+        img: "https://www.monin.com/us/media/catalog/product/cache/715e3bd7e68644be70dcb763021c03d6/3/8/38574fe5-b6d5-4ea7-9066-029cff1f98f7-2.png"
     },
     {
         name: "Iste",
         desc: "Jeg vil sove",
         price: 47,
         cat: "kaffe",
-        img: "https://static.wixstatic.com/media/159d55_c47eefa4b2384d5a89376aafb703339a~mv2.png/v1/fit/w_500,h_500,q_90/file.png"
+        img: "https://www.cfacdn.com/img/order/menu/Online/Drinks/tea_pdp.png"
     },
 ]
 
@@ -77,49 +77,49 @@ let dessert = [
         desc: "Hardt på utsiden, myk inni",
         price: 44,
         cat: "dessert",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://www.breadsmith.com/wp-content/uploads/2015/09/Brownie.png"
     },
     {
         name: "Oreokake",
         desc: "2 favoritter i en",
         price: 54,
         cat: "dessert",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://3brothersbakery.com/wp-content/uploads/2018/05/Cake8Oreo1.png"
     },
     {
         name: "Kanelbolle",
         desc: "Smaker jul",
         price: 46,
         cat: "dessert",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://harvestbakery.com/wp-content/uploads/2016/04/CinnamonBunw_CCIcing_clipped_rev_1.png"
     },
     {
         name: "Croissant",
         desc: "Fransk",
         price: 38,
         cat: "dessert",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://medias.bridordefrance.com/media/sys_master/images/h58/h5e/8801910063134.png"
     },
     {
         name: "Chiapudding",
         desc: "God.",
         price: 42,
         cat: "dessert",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://eventsand.co/wp-content/uploads/2020/02/Events-and-Co-Catering-Matcha-Chia-Pudding.png"
     },
     {
         name: "Brioche",
         desc: "Vet ikke helt hva dette er.",
         price: 44,
         cat: "dessert",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://assets.stickpng.com/thumbs/58b16cef102ddecdee0dd025.png"
     },
     {
         name: "Scones",
         desc: "Rart rundstykke",
         price: 37,
         cat: "dessert",
-        img: "https://www.drinktrade.com/img/onboarding/question-1.png"
+        img: "https://assets.stickpng.com/thumbs/5895fc35cba9841eabab609d.png"
     },
 ]
 
