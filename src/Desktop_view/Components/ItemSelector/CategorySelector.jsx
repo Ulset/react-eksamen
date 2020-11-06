f52e2eaa32c41f78ecf9de54c643d6b7364bd0ec
Sander Ulset <sanderulset1@gmail.com>
2020-11-06 23:11:17 +0100

    kan endre kategori

--- src/Desktop_view/Components/ItemSelector/CategorySelector.jsx
+++ src/Desktop_view/Components/ItemSelector/CategorySelector.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
 const CategorySelector = (props) => {
+    const changeCat = (newCat) => {
+        props.changeCat(newCat)
+    }
     return (
         <div className="desktop_cat_change">
-            <div className="base_cat_button selected"><p>Kaffe</p></div>
-            <div className="base_cat_button"><p>Dessert</p></div>
+            <div className={`base_cat_button${props.currCat === "kaffe" ? ' selected' : ''}`} onClick={()=>{changeCat("kaffe")}}>
+                <p>Kaffe</p>
+            </div>
+            <div className={`base_cat_button${props.currCat === "dessert" ? ' selected' : ''}`} onClick={()=>{changeCat("dessert")}}>
+                <p>Dessert</p>
+            </div>
             <div></div>
-            <div className="base_cat_button"><p>Tidligere bestillinger</p></div>
+            <div className={`base_cat_button${props.currCat === "tidligere_bestillinger" ? ' selected' : ''}`} onClick={()=>{changeCat("tidligere_bestillinger")}}>
+                <p>Tidligere bestillinger</p>
+            </div>
         </div>
     )
 }
