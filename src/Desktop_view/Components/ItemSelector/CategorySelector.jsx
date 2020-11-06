72bacb6a4ad5e3d23bba481cd323807343c4e0ee
Sander Ulset <sanderulset1@gmail.com>
2020-11-06 23:02:22 +0100

    css og startet på cat change

--- src/Desktop_view/Components/ItemSelector/CategorySelector.jsx
+++ src/Desktop_view/Components/ItemSelector/CategorySelector.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const CategorySelector = (props) => {
     return (
-        <div>
+        <div className="desktop_cat_change">
+            <div className="base_cat_button selected"><p>Kaffe</p></div>
+            <div className="base_cat_button"><p>Dessert</p></div>
             <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className="base_cat_button"><p>Tidligere bestillinger</p></div>
         </div>
     )
 }
--- src/Desktop_view/Components/ItemSelector/ItemSelectorDesktop.jsx
+++ src/Desktop_view/Components/ItemSelector/ItemSelectorDesktop.jsx
@@ -5,19 +5,30 @@ import ItemComponent from './ItemComponent'
 class ItemSelectorDesktop extends React.Component {
     constructor(props){
         super();
+        this.state = {
+            cat: "kaffe"
+        }
     }
 
     getItems = ()=> {
         return this.props.items.map((el, key) => {
             let isPLacedInCart = this.props.cart.includes(el);
-            return <ItemComponent item={el} key={key} addToCartFunc={this.props.addToCart} removeFromCart={this.props.removeFromCart} isPlacedInCart={isPLacedInCart}/>
+            if(this.state.cat == el.cat){
+                return <ItemComponent item={el} key={key} addToCartFunc={this.props.addToCart} removeFromCart={this.props.removeFromCart} isPlacedInCart={isPLacedInCart}/>
+            }
+        })
+    }
+
+    changeCat = (newCat) => {
+        this.setState({
+            cat: newCat
         })
     }
 
     render = () => {
         return (
             <div className="desktop_item_selector_container desktop_main_content">
-                <CategorySelector />
+                <CategorySelector currCat={this.state.cat} changeCat={this.changeCat} />
                 <div className="desktop_item_components_container">
                     {this.getItems()}
                 </div>
